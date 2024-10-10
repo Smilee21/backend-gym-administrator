975d005b8b26c9238985324cefb14e6afe0e2a1c
Smilee21 <antomigel23@gmail.com>
2024-10-10 12:23:23 -0400

    update migrations

--- src/migrations/1728576256253-init.ts
+++ src/migrations/1728576256253-init.ts
@@ -1,21 +1,21 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1728079541072 implements MigrationInterface {
-    name = 'Init1728079541072'
+export class Init1728576256253 implements MigrationInterface {
+    name = 'Init1728576256253'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`email\` varchar(255) NOT NULL, \`first_name\` varchar(255) NOT NULL, \`last_name\` varchar(255) NOT NULL, \`role\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`trainer\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`specialty\` varchar(255) NOT NULL, \`contact_info\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`client\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`contact_info\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`booking\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`booking_time\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`session_id\` int NULL, \`client_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`training_session\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`trainer_id\` int NOT NULL, \`day\` enum ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL, \`hour\` time NOT NULL, \`spaces\` int NOT NULL, \`duration\` time NOT NULL, \`trainer_id_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`training_session\` (\`id\` int NOT NULL AUTO_INCREMENT, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`day\` enum ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL, \`hour\` time NOT NULL, \`spaces\` int NOT NULL, \`duration\` time NOT NULL, \`trainer_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`booking\` ADD CONSTRAINT \`FK_f82cd0c042c5756ba7530063b6c\` FOREIGN KEY (\`session_id\`) REFERENCES \`training_session\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`booking\` ADD CONSTRAINT \`FK_65f5f7fdebd59a3289ee2f77b73\` FOREIGN KEY (\`client_id\`) REFERENCES \`client\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`training_session\` ADD CONSTRAINT \`FK_204a434ffdcf49a07b8970c8eb6\` FOREIGN KEY (\`trainer_id_id\`) REFERENCES \`trainer\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`training_session\` ADD CONSTRAINT \`FK_3bf02afe7d12617e37250440e5e\` FOREIGN KEY (\`trainer_id\`) REFERENCES \`trainer\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`training_session\` DROP FOREIGN KEY \`FK_204a434ffdcf49a07b8970c8eb6\``);
+        await queryRunner.query(`ALTER TABLE \`training_session\` DROP FOREIGN KEY \`FK_3bf02afe7d12617e37250440e5e\``);
         await queryRunner.query(`ALTER TABLE \`booking\` DROP FOREIGN KEY \`FK_65f5f7fdebd59a3289ee2f77b73\``);
         await queryRunner.query(`ALTER TABLE \`booking\` DROP FOREIGN KEY \`FK_f82cd0c042c5756ba7530063b6c\``);
         await queryRunner.query(`DROP TABLE \`training_session\``);
